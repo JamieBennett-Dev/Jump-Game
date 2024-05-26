8b10a45d02f490e7e8962e4276a25c99556405ba
Jamie Bennett <jbennett.130602@gmail.com>
2024-05-26 12:47:32 +0000

    add scoreboard

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,7 @@
 const game = document.getElementById('game');
 const character = document.getElementById('character');
 const block = document.getElementById('block');
+let counter = 0;
 
 // gif variable
 const gifs = [
@@ -21,3 +22,19 @@ function jump() {
     }
 }
 game.addEventListener('click', jump); // once the game is clicked, the jump function occurs
+
+// check if dead
+const checkDead = setInterval(function() {
+    const characterTop = parseInt(window.getComputedStyle(character).getPropertyValue('top'));
+    const blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue('left'));
+
+    if (blockLeft < 20 && blockLeft > 0 && characterTop > 130) { // if you hit the block
+        block.style.animation = 'none';
+        alert('YOU LOSE! score: '+Math.floor(counter/100));
+        counter = 0;
+        block.style.animation = 'block 1s infinite linear'
+    } else { // else you DON'T hit the block score increases
+        counter++;
+        document.getElementById('scoreSpan').innerHTML = Math.floor(counter/100);
+    }
+},10);
